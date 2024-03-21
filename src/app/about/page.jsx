e76d360c2f7a5abf49818ee8f79e38e357baaa71
4K45H107 <fahad11.ahmed11@gmail.com>
2024-03-21 10:31:30 +0600

    about page done -- brain animation problem

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,20 @@
 "use client";
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useRef } from "react";
+import { motion, useInView, useScroll } from "framer-motion";
+import Brain from "@/components/brain";
+import Image from "next/image";
 
 const AboutPage = () => {
+  const containerRef = useRef();
+  const { scrollYProgress } = useScroll({ container: containerRef });
+
+  //console.log(scrollYProgress);
+
+  const skillRef = useRef();
+  const isSkillRefInView = useInView(skillRef, { margin: "-100px" });
+
+  const experienceRef = useRef();
+  const isExperienceRefInView = useInView(experienceRef, { margin: "-100px" });
   return (
     <motion.div
       className="h-full"
@@ -10,10 +22,10 @@ const AboutPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="">
+      <div className="h-full overflow-scroll lg:flex" ref={containerRef}>
         {/* TEXT CONTAINER */}
 
-        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
+        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:w-1/2">
           {/* BIOGRAPHY CONTAINER */}
           <div className="flex flex-col gap-12 justify-center">
             {/* BIOGRAPHY TITLE */}
@@ -41,13 +53,16 @@ const AboutPage = () => {
                 <path
                   d="M2 87.9458C2 82.9663 3.50166 78.33 5.16667 73.668C9.53692 61.4313 14.5581 49.5143 20 37.7235C23.9347 29.1983 26.5296 20.344 30 11.668C31.2165 8.62679 33.2339 -0.529482 33.9444 2.66799C35.8864 11.407 33.4382 22.7261 33.2222 31.5013C32.7283 51.5685 32.7071 71.8018 34.2222 91.8347C34.5698 96.4298 35 100.845 35 105.446C35 112.646 38.8349 94.5079 39 93.2791C41.3576 75.7277 40.2147 57.1471 45 40.0569C46.1269 36.0322 47.5033 32.133 48.7778 28.168C49.3677 26.3328 49.495 26.332 48.7778 27.9458C45.6454 34.9937 46.3838 43.7891 45.5 51.3902C44.977 55.8883 45.4383 60.487 44.7778 64.9458C44.4793 66.9607 43.2376 70.1787 44.0556 72.2235C44.3086 72.8562 45.007 71.246 45.4444 70.7235C47.4624 68.3132 49.7966 66.1811 52 63.9458C55.0129 60.8892 57.6172 57.5681 60.7778 54.7235C67.4647 48.7053 50.4449 69.4863 44.6111 76.3347C37.8666 84.2522 44.9329 81.0689 51.7778 82.9458C56.1234 84.1373 61.1502 86.8835 64 90.4458C66.1513 93.1348 72.5503 79.2001 73 77.9458C73.6682 76.0817 77.4926 65.9458 73.5 65.9458C70.5059 65.9458 69.1218 66.758 69 70.168C68.9657 71.1297 67.2957 82.3984 68.2222 82.5013C72.0117 82.9224 72 75.1949 72 73.4458C72 70.2817 72.1618 67.2215 71.2222 64.168C70.7599 62.6654 73.9772 67.959 74.7778 68.9458C76.6214 71.2181 79.7559 78.7222 83.5 77.8902C87.0443 77.1026 85.0707 60.3514 85 57.168C84.9732 55.9619 83.6354 53.6522 85.5556 54.0013C88.3796 54.5148 90.8708 56.6012 92.4444 58.8902C97.0654 65.6116 95.1133 75.1564 92.7778 82.7235C92.4355 83.8325 90.1111 90.6846 90.1111 89.7235C90.1111 85.1113 93.0583 80.2858 94.4444 76.0013C98.615 63.1104 97.3187 49.314 95.0556 36.1124C94.8142 34.7048 91.6871 13.2864 92 23.6124C92.7392 48.0053 103.267 71.189 108.444 94.7235C109.089 97.6517 110.825 100.973 111 103.946C111.058 104.931 112.108 98.4295 112.222 97.5013C112.492 95.3148 118.738 66.4076 120.5 67.1124C123.278 68.2236 125.45 81.7926 126.778 84.9458C127.608 86.9181 127.188 86.4732 129.167 84.8902C133.868 81.129 139.547 78.3997 145 75.9458C147.79 74.6903 159 72.4563 159 68.9458"
                   stroke="black"
-                  stroke-width="3"
-                  stroke-linecap="round"
+                  strokeWidth="3"
+                  strokeLinecap="round"
                 />
               </svg>
             </div>
             {/* BIOGRAPHY SCROLL SVG */}
-            <svg
+            <motion.svg
+              initial={{ opacity: 0.2, y: 0 }}
+              animate={{ opacity: 1, y: "10px" }}
+              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
@@ -65,15 +80,25 @@ const AboutPage = () => {
                 stroke="#000000"
                 strokeWidth="1"
               ></path>
-            </svg>
+            </motion.svg>
           </div>
 
-          {/* SKILLS CONTAINER */}
-          <div className="flex flex-col gap-12 justify-center">
+          <div className="flex flex-col gap-12 justify-center" ref={skillRef}>
             {/* SKILL TITLE */}
-            <h1 className="font-bold text-2xl">SKILL</h1>
+            <motion.h1
+              initial={{ x: "-300px" }}
+              animate={isSkillRefInView ? { x: 0 } : {}}
+              transition={{ delay: 0.2 }}
+              className="font-bold text-2xl"
+            >
+              SKILLS
+            </motion.h1>
             {/* SKILL LIST */}
-            <div className="flex gap-4 flex-wrap">
+            <motion.div
+              initial={{ x: "-300px" }}
+              animate={isSkillRefInView ? { x: 0 } : {}}
+              className="flex gap-4 flex-wrap"
+            >
               <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
                 JavaScript
               </div>
@@ -95,9 +120,12 @@ const AboutPage = () => {
               <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
                 CodeForces
               </div>
-            </div>
+            </motion.div>
             {/* SKILL SVG */}
-            <svg
+            <motion.svg
+              initial={{ opacity: 0.2, y: 0 }}
+              animate={{ opacity: 1, y: "10px" }}
+              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
@@ -115,17 +143,131 @@ const AboutPage = () => {
                 stroke="#000000"
                 strokeWidth="1"
               ></path>
-            </svg>
+            </motion.svg>
           </div>
 
           {/* EXPERIENCE CONTAINER */}
-          <div lassName="flex flex-col justify-center">
+          <div
+            className="flex flex-col gap-12 justify-center pb-48"
+            ref={experienceRef}
+          >
             {/* EXPERIENCE TITLE */}
-            <h1 className="font-bold text-2xl">EXPERIENCE</h1>
+            <motion.h1
+              initial={{ x: "-350px" }}
+              animate={isExperienceRefInView ? { x: "0" } : {}}
+              transition={{ delay: 0.2 }}
+              className="font-bold text-2xl"
+            >
+              EXPERIENCE
+            </motion.h1>
+            {/* EXPERIENCE LIST */}
+            <motion.div
+              initial={{ x: "-300px" }}
+              animate={isExperienceRefInView ? { x: "0" } : {}}
+              className=""
+            >
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                <div className="w-1/3 ">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Senior JavaScript Engineer
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="p-3 text-sm italic">
+                    I led web development, offering expertise in JavaScript
+                    frameworks.{" "}
+                  </div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2024 - Present
+                  </div>
+                  {/* JOB COMPANY */}
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    Apple
+                  </div>
+                </div>
+                {/* CENTER */}
+                <div className="w-1/6 flex justify-center">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-1/3 "></div>
+              </div>
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                <div className="w-1/3 "></div>
+                {/* CENTER */}
+                <div className="w-1/6 flex justify-center">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-1/3 ">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Senior React Developer
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="p-3 text-sm italic">
+                    I spearheaded React-based application development,
+                    leveraging advanced skills.{" "}
+                  </div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2019 - 2024{" "}
+                  </div>
+                  {/* JOB COMPANY */}
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    Microsoft
+                  </div>
+                </div>
+              </div>
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                <div className="w-1/3 ">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Freelancer{" "}
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="p-3 text-sm italic">
+                    I provided web solutions, applying a range of technologies
+                    to address client requirements.{" "}
+                  </div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2010 - 2019{" "}
+                  </div>
+                </div>
+                {/* CENTER */}
+                <div className="w-1/6 flex justify-center">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-1/3"></div>
+              </div>
+            </motion.div>
           </div>
         </div>
         {/* SVG CONTAINER */}
-        <div className="hidden"></div>
+        <div className="hidden lg:block w-1/3 bg-red-400 sticky top-0 z-30 xl:w-1/2">
+          <Brain scrollYProgress={scrollYProgress} />
+        </div>
       </div>
     </motion.div>
   );
