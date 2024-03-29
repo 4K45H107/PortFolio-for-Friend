d5cba389771237f86b9d2bf617eb2f979a9d5232
4K45H107 <fahad11.ahmed11@gmail.com>
2024-03-30 02:09:17 +0600

    key in array of title, suggested by mohaimin

--- src/app/portFolio/page.jsx
+++ src/app/portFolio/page.jsx
@@ -8,7 +8,7 @@ const items = [
     id: 1,
     key: 1,
     color: "from-red-300 to-blue-300",
-    title: ["BJT Circuit Navigator", <br />, "Numerical Solution"],
+    title: ["BJT Circuit Navigator", "Numerical Solution"],
     desc: "This project aims at solving some BJT oriented circuits. It will take some inputs from the user as netlist, then the data will be processed using numerical methods in MATLAB and the output will be showed in schematic. It will use the mostly used circuits in our analysis. Here this will do AC and DC analysis of  BJT",
     img: "/1.png",
     link: "",
@@ -18,7 +18,7 @@ const items = [
     id: 2,
     key: 2,
     color: "from-blue-300 to-violet-300",
-    title: ["PID Controller", <br />, "Using OP-AMP"],
+    title: ["PID Controller", "Using OP-AMP"],
     desc: "This project's goal is to materialize this purpose using Op-Amp based PID controllers. It calculates an error value, e(t) as the difference between a desired set point (SP) and a measured process variable (PV) and applies a correction for that. PID is a method used in control systems and if tuned properly, can be incredibly effective and accurate. PID stands for Proportional, Integral and Derivative, three separate parts joined together. Based on the use case, we could have just P control, PI control or PD control. There are three PID Controller parameters in total. Each of them has their respective Op-Amp based implementation.",
     img: "/2.png",
     link: "",
@@ -28,7 +28,7 @@ const items = [
     id: 3,
     key: 3,
     color: "from-violet-300 to-purple-300",
-    title: "Autotransformer",
+    title: ["Autotransformer"],
     desc: "The project aims to design, build, and test a 240V/260V step-up autotransformer with four tapping. It is designed to convert a primary voltage of 240V to secondary voltages of 260V, 220V, 180V, and 110V. The autotransformer was developed to meet essential electrical engineering standards for reliability, and safety. The implementation phase involved real construction, testing, and data collection, followed by an in-depth evaluation of the design's efficacy. The autotransformer met all design criteria, with output voltages deviating from expected values by no more than 1%. This project serves as a good reference for building similar step-up autotransformers, shining light on crucial design issues, material selection, and assembly processes.",
     img: "/3.png",
     link: "",
@@ -38,7 +38,7 @@ const items = [
     id: 4,
     key: 4,
     color: "from-purple-300 to-red-300",
-    title: ["Reactive Power Compensation", <br />, "In Grid Connected PV Cell"],
+    title: ["Reactive Power Compensation", "In Grid Connected PV Cell"],
     desc: "This project investigates the application of reactive power compensation techniques in grid connected photovoltaic (PV) systems using Static Synchronous Compensators (STATCOMs) and fixed capacitor banks to reduce active and reactive power demand from the grid by supplying variable reactive power from an alternative source, thereby decreasing reliance on utility power and facilitating renewable energy integration. System modeling and analysis are conducted. The system integrates various load dynamics and changing solar irradiance conditions are simulated to assess the system's performance and response to disturbances. This project proposes a design methodology for reactive compensation in PV-integrated grid systems employing both STATCOMs and fixed capacitor banks.",
     img: "/4.png",
     link: "",
@@ -50,9 +50,7 @@ const items = [
     color: "from-red-300 to-blue-300",
     title: [
       "Boimetric Authentication System",
-      <br />,
       "For Classroom Management",
-      <br />,
       "Through Voice Recognition",
     ],
     desc: "This project aims to develop a biometric authentication system for classroom management through voice recognition technology. The proposed system utilizes advanced signal processing techniques, including Mel-Frequency Cepstral Coefficients (MFCC) for efficient feature extraction from voice samples. Machine learning algorithms, such as k-Nearest Neighbors (KNN) is employed for speaker modeling and classification. The system is designed to accommodate text-dependent speaker recognition. Robust audio preprocessing methods, including noise reduction and voice activity detection, are incorporated to improve the system's performance in real-world classroom environments.",
@@ -66,9 +64,7 @@ const items = [
     color: "from-blue-300 to-violet-300",
     title: [
       " EcoWatch: Smart Greenhouse",
-      <br />,
       "Environment Management",
-      <br />,
       "with Fire Safety",
     ],
     desc: "TThe emergence of the Internet of Things (IoT) has paved the way for remote monitoring and visualization of environmental parameters through wireless sensors, facilitating real-time data transmission and analysis. Our project, EcoWatch: Smart Greenhouse Environment Management with Fire Safety, leverages IoT principles to enhance the functionality and efficiency of traditional home appliances, focusing on greenhouse environment management and fire safety. Using an Arduino UNO board and an ESP8266 Wi-Fi Module, we establish a network for processing and transferring data from sensors to the Thingspeak Cloud, a versatile open API service.",
@@ -105,7 +101,11 @@ const PortfolioPage = () => {
               >
                 <div className="flex flex-col gap-8 text-white">
                   <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-6xl">
-                    {item.title}
+                    {item.title.map((title, index) => (
+                      <div key={index}>
+                        {title} <br />
+                      </div>
+                    ))}
                   </h1>
                   <div className="relative h-60 md:w-96 md:h-50 lg:w-[500px] lg:h-[300px] xl:w-[600px] xl:h-[350px]">
                     <Image
