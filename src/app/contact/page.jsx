39acf499ebf5d024f49f53f965657cf5a0cf4c74
4K45H107 <fahad11.ahmed11@gmail.com>
2024-03-30 02:39:37 +0600

    hopefully final

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -42,7 +42,7 @@ const ContactPage = () => {
     >
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* TEXT CONTAINER */}
-        <div className="h-1/3 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+        <div className="h-1/4 md:h-1/3 lg:h-full lg:w-1/2 flex items-center justify-center text-2xl md:text-3xl lg:text-6xl">
           <div>
             {text.split("").map((letter, index) => (
               <motion.span
--- src/components/navLinks.jsx
+++ src/components/navLinks.jsx
@@ -9,7 +9,7 @@ const NavLinks = ({ link }) => {
   return (
     <div className="">
       <Link
-        className={`rounded px-4 py-2  ${
+        className={`rounded px-4 py-2 text-lg ${
           pathName === link.url && "bg-black text-white"
         }`}
         href={link.url}
